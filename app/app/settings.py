c5c0bc8b3440ac41dadb18c8f819082b06babe89
aprianimmanuel <immanuelaprian@gmail.com>
2023-11-24 12:02:46 +0700

    update /app/settings.py following PEP8

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-mue*)ajby1=mdobpm@=@=)i-+8lrb-2725ayy%_=2fgq%nxr2*'
-)
+SECRET_KEY = 'django-insecure-mue*)ajby1=mdobpm@=@=)i-+8lrb-2725ayy%_=2fgq%nxr2*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
