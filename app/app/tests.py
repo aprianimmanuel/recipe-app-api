4df3f1a048d27e93e62334c2dd2519e127df92cc
aprianimmanuel <immanuelaprian@gmail.com>
2023-11-24 13:25:13 +0700

    demo TDD

---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""
+Sample Tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc moduke"""
+
+    def test_add_numbers(self):
+        """Test that two numbers are added together"""
+        res = calc.add(3,8)
+        self.assertEqual(res, 11)
+
+    def test_substract_number(self):
+        """Test that values are substracted and returned"""
+        res = calc.substract(5,11)
+        self.assertEqual(res, 6)
