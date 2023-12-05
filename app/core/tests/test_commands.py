d91a24964f915100417ba3a8340deaa3302478db
aprianimmanuel <immanuelaprian@gmail.com>
2023-12-05 11:22:08 +0700

    update unit test, waitfordb, and tests

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,7 +3,7 @@ Test Custom Django Management Commands
 """
 from unittest.mock import patch
 
-from pyscopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -17,5 +17,16 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for db when db is available"""
         patched_check.return_value = True
+
         call_command('wait_for_db')
+
         patched_check.assert_called_once_with(database=['default'])
+
+    @patch('time.sleep')
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for db when getting OperationalError"""
+        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True] # noqa
+
+        call_command('wait_for_db')
+        self.assertEqual(patched_check.call_count, 6)
+        patch_check.assert_called_with(database=['default'])
