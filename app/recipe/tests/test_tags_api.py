e85b878d9141e837dcca8780e17210f3e27a5f31
aprianimmanuel <immanuelaprian@gmail.com>
2023-12-18 13:16:14 +0700

    update unit test for tags API

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -30,6 +30,7 @@ class PublicTagsApiTests(TestCase):
     def setUp(self):
         """Setup the test client."""
         self.client = APIClient()
+        self.user = create_user(email='test@example.com', password='testpass123')  # noqa
 
     def test_auth_required(self):
         res = self.client.get(TAGS_URL)
@@ -41,6 +42,7 @@ class PrivateTagsApiTests(TestCase):
     """Test authenticated API requests."""
 
     def setUp(self):
+        """Setup the test client."""
         self.user = create_user()
         self.client = APIClient()
         self.client.force_authenticate(self.user)
@@ -54,21 +56,22 @@ class PrivateTagsApiTests(TestCase):
 
         tags = Tag.objects.all().order_by('-name')
         serializer = TagSerializer(tags, many=True)
+
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
     def test_tags_limited_to_user(self):
-        """Test list of tags is limited to authenticated user."""
+        """Test that tags returned are for the authenticated user."""
         user2 = create_user(email='user2@example.com')
-        tag = Tag.objects.create(user=self.user, name='Fruity')
-        Tag.objects.create(user=user2, name='Comfort Food')
+        Tag.objects.create(user=user2, name='Fruity')
+        tag = Tag.objects.create(user=self.user, name='Comfort Food')
 
         res = self.client.get(TAGS_URL)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
-        self.assertEqual(res.data[0]['id'], self.user.id)
+        self.assertEqual(res.data[0]['id'], tag.id)
 
     def test_update_tag(self):
         """Test updating a tag."""
@@ -76,7 +79,9 @@ class PrivateTagsApiTests(TestCase):
         payload = {'name': 'Dessert'}
 
         url = detail_url(tag.id)
-        self.client.patch(url, payload)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
 
         tag.refresh_from_db()
         self.assertEqual(tag.name, payload['name'])
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -40,7 +40,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
     """
     ViewSet for tags.
     """
