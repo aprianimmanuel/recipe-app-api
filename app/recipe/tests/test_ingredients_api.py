3dcebf4909cf6844441b07a7633b800e80178ed2
aprianimmanuel <immanuelaprian@gmail.com>
2024-02-02 10:24:54 +0700

    correct typo

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
 
-from test_framework import status
+from rest_framework import status
 from rest_framework.test import APIClient
 
 from core.models import Ingredient
